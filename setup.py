f3b9796e4402256a8c84ab1724c64b08cb288970
Tarik02 <Taras.Fomin@gmail.com>
2022-01-20 17:18:35 +0200

    version 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='localhttps',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_reqs,

bc46b81f0ea3acd6371ac55185ea6fb708a50e8a
Tarik02 <Taras.Fomin@gmail.com>
2022-03-30 10:02:44 +0300

    an option to customize data path

--- localhttps/__init__.py
+++ localhttps/__init__.py
@@ -1,6 +1,5 @@
 import asyncio
 import asyncclick as click
-from aiopath import AsyncPath
 from rich.console import Console
 from localhttps.app import App
 from localhttps.cli.commands import cli
@@ -12,9 +11,6 @@ async def create_context():
     ctx = Context()
     ctx.console = Console(soft_wrap=True)
     ctx.cmd = DefaultConsoleCmd(ctx.console)
-    ctx.data_path = (await AsyncPath.home())/'.config'/'localhttps'
-    app = App(ctx.data_path)
-    ctx.app = app
     ctx.keychain = get_current_keychain()
     return ctx
 
--- localhttps/cli/commands/__init__.py
+++ localhttps/cli/commands/__init__.py
@@ -1,13 +1,42 @@
+from typing import Optional
 import asyncclick as click
+import os
+from aiopath import AsyncPath
+from localhttps.app import App
 from localhttps.cli import with_context, Context
 from localhttps.cmd import DefaultConsoleCmd
 
 
+async def resolve_default_data_path() -> AsyncPath:
+    data_path = os.environ.get('LOCALHTTPS_DATA', default=None)
+
+    if data_path is not None:
+        data_path = await AsyncPath(data_path).absolute()
+    else:
+        config_home = os.environ.get('XDG_CONFIG_HOME', default=None)
+        if config_home is not None:
+            config_home = await AsyncPath(config_home).absolute()
+        else:
+            config_home = await ((await AsyncPath.home())/'.config').absolute()
+
+        data_path: AsyncPath = config_home/'localhttps'
+
+    await data_path.mkdir(exist_ok=True)
+    return data_path
+
 @click.group()
+@click.option('--data-path', default=None, help='Base data path (defaults to $LOCALHTTPS_DATA or $XDG_CONFIG_HOME/localhttps (~/.config/localhttps))')
 @click.option('--ca', default='default', help='Certification authority name')
 @click.option('--verbose/-V', default=False, help='Verbose output')
 @with_context
-async def cli(ctx: Context, ca: str, verbose: bool):
+async def cli(ctx: Context, data_path: Optional[str], ca: str, verbose: bool):
+    if data_path is not None:
+        ctx.data_path = await AsyncPath(data_path).absolute()
+    else:
+        ctx.data_path = await resolve_default_data_path()
+
+    ctx.app = App(ctx.data_path)
+
     ctx.ca = ctx.app.ca(ca)
     if verbose and isinstance(ctx.cmd, DefaultConsoleCmd):
         ctx.cmd.verbose = True
